5a5856f88306604e5e726cb7f637af18a1ac8399
(Student  C00298918) Harry Knight <C00298918@setu.ie>
2025-01-21 12:26:58 +0000

    first commit

---
+++ 1D Pacman/SFML Template 2.6.12/Game.h
@@ -0,0 +1,38 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+
+int screenWidth = 800;
+int screenHeight = 600;
+int dotsEaten = 0;
+int dotReset = 20;
+
+float rectWidth = 30;
+float rectHeight = 30;
+
+float xPosition = rand() % 800;
+float yPosition = 200;
+float speed = 5;
+bool spacePressed = false;
+
+const int numCircles = 20;
+sf::CircleShape circles[numCircles];
+
+sf::Time timePerFrame = sf::seconds(1.0f / 60.0f);
+sf::Time timeSinceLastUpdate = sf::Time::Zero;
+sf::Clock clockForFrameRate;
+
+sf::String m_counter{ "0" };
+sf::Font m_ArialBlackfont;
+sf::Text m_pointCount;
+sf::RectangleShape simpleRectangle;
+
+void setupFontAndText();
+void update();
+void orbCreation();
+#endif
---
+++ 1D Pacman/SFML Template 2.6.12/main.cpp
@@ -0,0 +1,164 @@
+//Noel O' Hara
+//first simple game. 
+
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h> 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Game.h"
+
+//recreate the game ..
+//
+//https://abagames.github.io/crisp-game-lib-11-games/?pakupaku
+// see blackboard for sample sprites.
+// 
+int main()
+{
+	setupFontAndText();
+	sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "1D Pacman");
+
+	simpleRectangle.setSize(sf::Vector2f(rectWidth, rectHeight));
+
+	simpleRectangle.setFillColor(sf::Color::Red);
+
+	simpleRectangle.setPosition(100, 200);
+
+	srand(time(NULL));
+
+	clockForFrameRate.restart();
+
+	while (window.isOpen())
+	{
+		update();
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		timeSinceLastUpdate += clockForFrameRate.restart();
+
+		if (timeSinceLastUpdate > timePerFrame)
+		{
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+			{
+				if (spacePressed == false)
+				{
+					speed *= -1;
+					spacePressed = true;
+				}
+			}
+			else {
+				spacePressed = false;
+			}
+
+			xPosition += speed;
+
+			if (xPosition > screenWidth)
+			{
+				xPosition = 0;
+			}
+			else if (xPosition + 30 < 0)
+			{
+				xPosition = screenWidth;
+			}
+
+			simpleRectangle.setPosition(xPosition, yPosition);
+
+			for (int index = 0; index < numCircles; index++)
+			{
+				if (simpleRectangle.getGlobalBounds().intersects(circles[index].getGlobalBounds()))
+				{
+					circles[index].setPosition(1000, 1000);
+					dotsEaten++;
+					dotReset--;
+
+				}
+			}
+
+			if (dotReset == 0)
+			{
+				orbCreation();
+				dotReset = 20;
+			}
+
+			window.clear();
+			for (int index = 0; index < numCircles; index++)
+			{
+				window.draw(circles[index]);
+			}
+			window.draw(simpleRectangle);
+			window.draw(m_pointCount);
+
+			window.display();
+
+			timeSinceLastUpdate = sf::Time::Zero;
+		}
+	}
+
+	return 0;
+}
+
+void setupFontAndText()
+{
+	orbCreation();
+	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	m_pointCount.setFont(m_ArialBlackfont);
+	m_pointCount.setStyle(sf::Text::Underlined | sf::Text::Italic | sf::Text::Bold);
+	m_pointCount.setPosition(40.0f, 40.0f);
+	m_pointCount.setCharacterSize(30U);
+	m_pointCount.setOutlineColor(sf::Color::Red);
+	m_pointCount.setFillColor(sf::Color::Black);
+	m_pointCount.setOutlineThickness(3.0f);
+
+}
+
+void update()
+{
+	m_counter = std::to_string(dotsEaten);
+	m_pointCount.setString(m_counter);
+}
+
+void orbCreation()
+{
+	int chosenDot = rand() % 20;
+
+	for (int index = 0; index < numCircles; index++)
+	{
+		if (index == chosenDot)
+		{
+			circles[index].setFillColor(sf::Color::Yellow);
+			circles[index].setRadius(15);
+			circles[index].setPosition(40 * index, 195);
+		}
+		else
+		{
+			circles[index].setFillColor(sf::Color::White);
+			circles[index].setRadius(10);
+			circles[index].setPosition(40 * index, 200);
+		}
+	}
+}
